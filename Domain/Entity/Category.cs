2345c3c918ce633b57a5f7ebc964c00c4305760e
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-06-17 15:09:17 +0430

    add domain models

---
+++ Domain/Entity/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Entity
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Parent { get; set; }
+        public IEnumerable<EBook> EBooks { get; set; }
+    }
+}
---
+++ Domain/Entity/EBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain.Entity
+{
+    class EBook
+    {
+        //Type
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExtention { get; set; }
+        public int Size { get; set; }
+        public string Path { get; set; }
+        public string Publisher { get; set; }
+        public string Writer { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+        public Uri GoodReadLink { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
