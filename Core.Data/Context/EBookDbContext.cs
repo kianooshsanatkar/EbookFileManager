e91c46de16ce97964c16bd16ee3f0f6727962704
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-06-18 02:57:38 +0430

    add EbookDbContext

---
+++ Core.Data/Context/EBookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Context
+{
+    public class EBookDbContext : DbContext
+    {
+        public DbSet<EBook> EBooks { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
